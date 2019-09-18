a8b95e01bf6982a292270e4d7b50ebf7a68d33f6
Adebola Adeola <debola_adeola@yahoo.com>
2019-09-18 19:03:33 +0100

    DO settings

--- blm/settings.py
+++ blm/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'l^fgheo_n(&98^cj_jbwe#gtggxp$@+*zqrxkjorzpa!!5)a9u'
+SECRET_KEY = '81pyuu9tg(n@90u-08=*!bs$v*5k$04s)s@sgyb#6^@y&8$09+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '206.81.26.92', 'www.budgetlikemagic.com', 'budgetlikemagic.com']
 
 
 # Application definition
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -78,12 +79,12 @@ WSGI_APPLICATION = 'blm.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'chris_db',
-        'USER': 'root',
-        'PASSWORD': 'SpEcSyutw4,!',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'blm_db',
+        'USER': 'debola',
+        'PASSWORD': 'Sp#c3yutw4,!',
         'HOST': 'localhost',
-        'PORT': '3306',
+        'PORT': '',
     }
 }
 
@@ -112,7 +113,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'Africa/Lagos'
+TIME_ZONE = 'UTC' #'Africa/Lagos'
 
 USE_I18N = True
 
@@ -124,7 +125,28 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/2.1/howto/static-files/
+
+AWS_ACCESS_KEY_ID = 'SXQ5KPNY4JYHJ4V3DG5Z'
+AWS_SECRET_ACCESS_KEY = 'PM+dpux9zQ0iRq9FcWRCsqwwcuYQBbW/tw7Klsj3NRE'
+
+AWS_STORAGE_BUCKET_NAME = 'blm-space'
+AWS_S3_ENDPOINT_URL = 'https://fra1.digitaloceanspaces.com'
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+AWS_LOCATION = 'static'
+AWS_DEFAULT_ACL = 'public-read'
+
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+STATIC_URL = '{}/{}/'.format(AWS_S3_ENDPOINT_URL, AWS_LOCATION)
+STATIC_ROOT = 'static/'
+
 
 LOGIN_REDIRECT_URL = '/sprout/' # goes here after logging in
 
@@ -136,13 +158,8 @@ LOGIN_EXEMPT_URLS = (
     r'^chris/reset_password/$',
     r'^chris/password_reset/done/$',
     r'^chris/password_reset/confirm/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$',
-    r'^chris/password_reset/complete/$',
-    r'^sprout/transfer/$'
+    r'^chris/password_reset/complete/$'
 )
-
-# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend' #This will avoid the need for an SMTP server as e-mails will be printed to the console. For more information, please refer to: https://docs.djangoproject.com/en/dev/ref/settings/#email-host
-# EMAIL_HOST = 'localhost'
-# EMAIL_PORT = 1025
 
 EMAIL_USE_TLS = True
 EMAL_HOST = 'smtp.zoho.eu'
@@ -151,3 +168,8 @@ EMAIL_HOST_PASSWORD = '8KkB8!xM19$$'
 EMAIL_PORT = 465
 
 DATE_INPUT_FORMATS = ('%d-%m-%Y','%Y-%m-%d')
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_SSL_REDIRECT = True

746ff35aa47cab6839c877472fdad45bf398c91f
Adebola Adeola <debola_adeola@yahoo.com>
2019-09-19 19:50:54 +0100

    no change

--- chris/views.py
+++ chris/views.py
@@ -6,7 +6,6 @@
 
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.shortcuts import redirect, render
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserChangeForm, PasswordChangeForm
